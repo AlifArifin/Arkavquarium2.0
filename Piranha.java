c499862693ddee3a7576e303c53d3da944b8f3ba
AlifArifin <m.alif.arifin@gmail.com>
2018-04-23 21:56:21 +0700

    update Piranha

--- Piranha.java
+++ Piranha.java
@@ -1,4 +1,5 @@
 import java.lang.Comparable;
+import java.lang.Math;
 import java.util.Random;
 
 /**
@@ -8,12 +9,14 @@ import java.util.Random;
  * @author Muhammad Alif Arifin/13516078
  * @version 23 April 2018
  */
-public class Piranha extends Fish implements Comparable {
+public class Piranha extends Fish implements Comparable<Piranha> {
   //Atribut
   protected String image;
-  protected static final int valuePiranha; //harga ikan piranha
-  protected static final int radiusPiranha;
-  protected static final String[] imagePiranha;
+  protected static final int valuePiranha = 300; //harga ikan piranha
+  protected static final int radiusPiranha = 35;
+  protected static final String[] imagePiranha = {
+    "img/LPiranha.png", "img/RPiranha.png", "img/LHungryPiranha.png", "img/RHungryPiranha.png"
+  };
   
   /**
    * Konstruktor piranha.
@@ -90,13 +93,13 @@ public class Piranha extends Fish implements Comparable {
     } else if (countMove >= deadTime) {
       return -2;
     } else if (changeMove <= 0 && !hungry) {
-      setchangeMove();
+      changeChangeMove();
     }
   
     if (hungry && !listGuppy.isEmpty()) {
-      return hungryMove(listGuppy);
+      return hungryMove(listGuppy, time);
     } else {
-      return notHungryMove();
+      return notHungryMove(time);
     }
   } 
   
@@ -104,15 +107,16 @@ public class Piranha extends Fish implements Comparable {
    * Method untuk pergerakan Piranha ketika sedang tidak lapar/
    * lapar namun tidak ada makanan.
    * 
+   * @param time time untuk menunjukkan waktu setelah pergerakan terakhir
    * @return int yang menunjukkan apabila -1 maka tidak ada makanan 
    *         yang dimakan
    */
-  public int notHungryMove() {
+  public int notHungryMove(double time) {
     Random rand = new Random();
-    double rad = PI / 180 * direction;
+    double rad = Math.PI / 180 * direction;
     
-    position.setX(position.getX() + speedFish * cos(rad) * time);
-    position.setY(position.getY() + speedFish * sin(rad) * time);
+    position.setX(position.getX() + speedFish * Math.cos(rad) * time);
+    position.setY(position.getY() + speedFish * Math.sin(rad) * time);
     
     if (position.isOutLeft(radiusPiranha)) {
       position.setX(radiusPiranha);
@@ -140,11 +144,12 @@ public class Piranha extends Fish implements Comparable {
    * dan saat itu ada makanan.
    * 
    * @param listGuppy merupakan list dari Guppy yang ada di aquarium
+   * @param time time untuk menunjukkan waktu setelah pergerakan terakhir
    * @return int yang menunjukkan apabila -1 maka tidak ada makanan yang 
    *         dimakan namun apabila bukan -1 menujukkan index Guppy (makanan) 
    *         pada listGuppy yang telah dimakan
    */
-  public int hungryMove(ListObj<Guppy> listGuppy) {
+  public int hungryMove(ListObj<Guppy> listGuppy, double time) {
     double closestFood = listGuppy.get(0).getPosition().distanceTo(position);
     int idxFood = 0;
     for (int i = 0; i < listGuppy.size(); i++) {
@@ -158,7 +163,7 @@ public class Piranha extends Fish implements Comparable {
     Guppy guppy = listGuppy.get(idxFood);
 
     double a = position.patan2(guppy.getPosition());
-    int dir = ((a * 180.0 / PI).intValue() % 360 + 360) % 360;
+    int dir = ((int)(a * 180.0 / Math.PI) % 360 + 360) % 360;
     setDirection(dir); 
     
     int temp = Guppy.getRadiusGuppy() * guppy.getPhase();
@@ -166,8 +171,8 @@ public class Piranha extends Fish implements Comparable {
       countMove = 0;
       return listGuppy.find(guppy);
     } else {
-      position.setX(position.getX() + speedFish * cos(a) * time);
-      position.setY(position.getY() + speedFish * sin(a) * time);
+      position.setX(position.getX() + speedFish * Math.cos(a) * time);
+      position.setY(position.getY() + speedFish * Math.sin(a) * time);
       return -1;
     }
   }
